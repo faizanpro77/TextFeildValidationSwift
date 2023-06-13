6cb785ac4a1c5b49600b75602db020dc59f18733
faizanpro77 <faizyshaikh204@gmail.com>
2023-06-13 16:56:14 +0530

    add functionality to take only input

--- TextFeildValidationSwift/TextFieldViewController.swift
+++ TextFeildValidationSwift/TextFieldViewController.swift
@@ -7,18 +7,27 @@
 
 import UIKit
 
-class TextFieldViewController: UIViewController {
+class TextFieldViewController: UIViewController,UITextFieldDelegate {
 
     @IBOutlet weak var checkTestFeild: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+       
+        checkTestFeild.delegate = self
         // Do any additional setup after loading the view.
     
         checkTestFeild.borderStyle = .line
     }
    
+    //MARK: only digits
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let allowedCharacters = CharacterSet.decimalDigits
+        let characterSet = CharacterSet(charactersIn: string)
+        return allowedCharacters.isSuperset(of: characterSet)
+        
+    }
     
     @IBAction func textFeildChanged(_ sender: Any) {
         print(checkTestFeild.text!)
